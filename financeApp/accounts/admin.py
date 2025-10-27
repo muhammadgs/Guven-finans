3a562dac7b73c79f0f1aa0932e88cdcf4cd14717
muhammadgs <mehemmedg2006@gmail.com>
2025-10-28 00:57:36 +0400

    Add entrepreneur registration form and model

--- financeApp/accounts/admin.py
+++ financeApp/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import OwnerRegistration
+
+
+@admin.register(OwnerRegistration)
+class OwnerRegistrationAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "company_name",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status", "created_at")
+    search_fields = (
+        "first_name",
+        "last_name",
+        "company_name",
+        "email",
+        "company_email",
+    )
+    ordering = ("-created_at",)
--- financeApp/accounts/urls.py
+++ financeApp/accounts/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("", views.register_choice, name="register_choice"),
-    path("sahibkar/", views.register_sahibkar, name="register_sahibkar"),
-    path("isci/", views.register_isci, name="register_isci"),
+    path("qeydiyyat/sahibkar/", views.register_owner, name="register-owner"),
+    path(
+        "qeydiyyat/sahibkar/təşəkkürlər/",
+        views.owner_thanks,
+        name="owner-thanks",
+    ),
 ]
